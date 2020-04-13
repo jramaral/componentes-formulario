cdfcbaa8fec95b7ffa7dffda9e176eb533d14c91
jramaral <j.robertoamaral@gmail.com>
2020-04-13 16:47:11 -0400

    inicio

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,248 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Formulários no Vue</h1>
+        <p class="lead">Treinando a manipulação de formulários</p>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <!-- formulario -->
+        <div class="col-sm-6">
+          <h3>Preencha abaixo</h3>
+
+          <form @submit.prevent="enviar">
+            <div class="form-group">
+              <label>Nome:</label>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Seu nome"
+                v-model.trim="desenvolvedor.nome"
+              />
+            </div>
+
+            <div class="form-group">
+              <label>Endereço de email:</label>
+              <input
+                type="email"
+                class="form-control"
+                placeholder="Seu email"
+                v-model.lazy="desenvolvedor.email"
+              />
+            </div>
+
+            <div class="form-group">
+              <label>Idade:</label>
+              <input
+                type="number"
+                class="form-control"
+                placeholder="Sua idade"
+                v-model.number="desenvolvedor.idade"
+              />
+            </div>
+
+            <div class="form-group">
+              <p>Gênero:</p>
+
+              <div class="form-check form-check-inline">
+                <input
+                  type="radio"
+                  class="form-check-input"
+                  v-model="desenvolvedor.genero"
+                  value="Masculino"
+                />
+                <label class="form-check-label">Masculino</label>
+              </div>
+
+              <div class="form-check form-check-inline">
+                <input
+                  type="radio"
+                  class="form-check-input"
+                  v-model="desenvolvedor.genero"
+                  value="Feminino"
+                />
+                <label class="form-check-label">Feminino</label>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label>Ocupação:</label>
+              <select class="form-control" v-model="desenvolvedor.ocupacao">
+                <option value="" disabled>Selecione uma opção...</option>
+                <option
+                  v-for="(ocupacao, indice) in ocupacoes"
+                  :key="indice"
+                  :value="ocupacao"
+                >
+                  {{ ocupacao }}
+                </option>
+              </select>
+            </div>
+
+            <div class="form-group">
+              <p>Tecnologias:</p>
+
+              <div class="form-check form-check-inline">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  value="JavaScript"
+                  v-model="desenvolvedor.tecnologias"
+                />
+                <label class="form-check-label">JavaScript</label>
+              </div>
+
+              <div class="form-check form-check-inline">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  v-model="desenvolvedor.tecnologias"
+                  value="Vue JS"
+                />
+                <label class="form-check-label">Vue JS</label>
+              </div>
+
+              <div class="form-check form-check-inline">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  value="Vuex"
+                  v-model="desenvolvedor.tecnologias"
+                />
+                <label class="form-check-label">Vuex</label>
+              </div>
+
+              <div class="form-check form-check-inline">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  value="Vue Router"
+                  v-model="desenvolvedor.tecnologias"
+                />
+                <label class="form-check-label">Vue Router</label>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label>Resumo de perfil:</label>
+              <textarea
+                class="form-control"
+                placeholder="Conte-nos um pouco sobre você..."
+                v-model="desenvolvedor.biografia"
+              ></textarea>
+            </div>
+
+            <div class="form-group">
+              <div class="form-check form-check-inline">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  v-model="desenvolvedor.notificacoes"
+                  true-value="Sim"
+                  false-value="Não"
+                />
+                <label class="form-check-label"
+                  >Receber notificações por email</label
+                >
+              </div>
+            </div>
+
+            <button class="btn btn-secondary">Resetar</button>
+            <button class="btn btn-success">Enviar</button>
+          </form>
+        </div>
+
+        <!-- saida -->
+        <div class="col-sm-6">
+          <h3>Saída</h3>
+
+          <div class="card-header">Dados</div>
+
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+              <strong>Nome: </strong>{{ desenvolvedor.nome }}
+            </li>
+            <li class="list-group-item">
+              <strong>Email: </strong>{{ desenvolvedor.email }}
+            </li>
+            <li class="list-group-item">
+              <strong>Idade: </strong>{{ desenvolvedor.idade }}
+            </li>
+            <li class="list-group-item">
+              <strong>Gênero: </strong>{{ desenvolvedor.genero }}
+            </li>
+            <li class="list-group-item">
+              <strong>Ocupação: </strong>{{ desenvolvedor.ocupacao }}
+            </li>
+            <li class="list-group-item">
+              <strong>Tecnologias: </strong>
+              <ul>
+                <li
+                  v-for="(tecnologia, indice) in desenvolvedor.tecnologias"
+                  :key="indice"
+                >
+                  {{ tecnologia }}
+                </li>
+              </ul>
+            </li>
+            <li class="list-group-item">
+              <strong>Biografia: </strong>
+              <div style="white-space: pre">{{ desenvolvedor.biografia }}</div>
+            </li>
+            <li class="list-group-item">
+              <strong>Receber notificações?</strong>
+              {{ desenvolvedor.notificacoes }}
+            </li>
+          </ul>
+
+          <div class="card-header">Model</div>
+
+          <div class="card-body">
+            <pre><code>{{ desenvolvedor }}</code></pre>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      desenvolvedor: {
+        nome: "",
+        email: "",
+        idade: 45,
+        biografia: "Eu sou um programador JavaScript",
+        genero: "Masculino",
+        tecnologias: [],
+        notificacoes: "Não",
+        ocupacao: ""
+      },
+      ocupacoes: [
+        "Desenvolvedor Front End (Web)",
+        "Desenvolvedor Front End (Mobile)",
+        "Desenvolvedor Front End (Web e Mobile)",
+        "Desenvolvedor Back End",
+        "Desenvolvedor Full Stack"
+      ]
+    };
+  },
+  methods: {
+    enviar(event) {
+      const formularioEnviado = Object.assign({}, this.desenvolvedor);
+      console.log("Formulario enviado! ", formularioEnviado);
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.btn {
+  margin-right: 5px;
 }
 </style>
